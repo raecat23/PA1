2606975b1b87c80c1b15c252f322ad65395b6cf2
raecat23 <43554898+raecat23@users.noreply.github.com>
2018-09-28 10:14:18 -0400

    FILESSSSSS pt2 electric boolagoo

---
+++ src/cs131/pa1/filter/sequential/ExitFilter.java
@@ -0,0 +1,13 @@
+package cs131.pa1.filter.sequential;
+
+public class ExitFilter extends SequentialFilter{
+
+	@Override
+	protected String processLine(String line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+
