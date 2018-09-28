8d1715e777b21ad7697597db8e7c0db679bdbfd2
MidgeWilliams <43554894+MidgeWilliams@users.noreply.github.com>
2018-09-28 10:40:06 -0400

    Splits Piping now

--- src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
+++ src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
@@ -1,10 +1,28 @@
 package cs131.pa1.filter.sequential;
 
-import java.util.List;
+import java.util.*;
 
 public class SequentialCommandBuilder {
 	
+	/**
+	 * 
+	 * @param command The user input string
+	 * @return The list of Sequential filters that need to be executed
+	 */
 	public static List<SequentialFilter> createFiltersFromCommand(String command){
+		List<SequentialFilter> cmds = new LinkedList<>();
+		boolean commandProcessed = false;
+		while(!commandProcessed) {
+			int endCmd = command.indexOf('|');
+			String cmd = "";
+			if(endCmd != -1) {
+				cmd = command.substring(0, endCmd - 1);
+				command = command.substring(endCmd+2);
+			} else {
+				cmd = command;
+				commandProcessed = true;
+			}
+		}		
 		return null;
 	}
 	
