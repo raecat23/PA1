261ba0b4e7cc9852ff4e24912636c0b446718bf7
raecat23 <43554898+raecat23@users.noreply.github.com>
2018-09-28 18:14:25 -0400

    CAT and WC Filters Kinda Done

--- src/cs131/pa1/filter/sequential/CATFilter.java
+++ src/cs131/pa1/filter/sequential/CATFilter.java
@@ -1,11 +1,32 @@
 package cs131.pa1.filter.sequential;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CATFilter extends SequentialFilter {
 
 	@Override
 	protected String processLine(String line) {
-		// TODO Auto-generated method stub
-		return null;
+		//errors to check: if its a file, if the file exists
+		String output = "";
+		if(this.prev == null /* || the input is a file and the file exists*/ ){
+			Scanner scan = new Scanner(System.in);
+			try {
+				scan = new Scanner(new File(line));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			while(scan.hasNextLine()){
+				output += scan.nextLine() + "\n";
+			}
+		}
+		else{
+			//error handling here
+			
+		}
+		return output;
 	}
 
 }
--- src/cs131/pa1/filter/sequential/WCFilter.java
+++ src/cs131/pa1/filter/sequential/WCFilter.java
@@ -1,11 +1,30 @@
 package cs131.pa1.filter.sequential;
 
 public class WCFilter extends SequentialFilter {
-
+	
 	@Override
 	protected String processLine(String line) {
-		// TODO Auto-generated method stub
-		return null;
+		int wordcount = 0;
+		int linecount = 0;
+		int charactercount = 0;
+		for(int i = 0; i< line.length(); i++){
+			String s =line.substring(i,  i+1);
+			if(s.equals("\\")){
+				if(s.substring(i+1, i+2).equals("n")){
+					linecount++;
+				}
+			}
+			else if(s.equals(" ")){
+				wordcount++;
+				charactercount++;
+			}
+			else{
+				charactercount++;
+			}
+		}
+		return Integer.toString(wordcount) + "      " + Integer.toString(linecount) + "      " + Integer.toString(charactercount);
+		//make sure to ignore \n
+		//how to add them together?
 	}
 
 }
