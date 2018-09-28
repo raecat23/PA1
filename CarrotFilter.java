7711ad227a940f7e168bec16cdfe63b97d731578
raecat23 <43554898+raecat23@users.noreply.github.com>
2018-09-28 10:12:03 -0400

    Add files via upload
    FILESSSSSS

---
+++ CarrotFilter.java
@@ -0,0 +1,12 @@
+package cs131.pa1.filter.sequential;
+
+public class CarrotFilter extends SequentialFilter{
+
+	@Override
+	protected String processLine(String line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
---
+++ WCFilter.java
@@ -0,0 +1,12 @@
+package cs131.pa1.filter.sequential;
+
+public class WCFilter extends SequentialFilter {
+
+	@Override
+	protected String processLine(String line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
