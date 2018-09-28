96e958077c153adf91fa7a196ce092498ce23aa3
MidgeWilliams <43554894+MidgeWilliams@users.noreply.github.com>
2018-09-28 09:48:44 -0400

    Update 3 (Post tutorial)

--- src/cs131/pa1/filter/sequential/SequentialREPL.java
+++ src/cs131/pa1/filter/sequential/SequentialREPL.java
@@ -2,13 +2,15 @@ package cs131.pa1.filter.sequential;
 
 import java.util.Scanner;
 
+import cs131.pa1.filter.Message;
+
 public class SequentialREPL {
 
 	static String currentWorkingDirectory;
 	String presentWorkingDirectory;
 	
 	public static void main(String[] args){
-		System.out.println(">Welcome to the Unix-like Command Line");
+		System.out.println(Message.WELCOME);
 
 		REPloop();
 	}
@@ -19,26 +21,12 @@ public class SequentialREPL {
 	public static void REPloop() {
 		
 		Scanner keyboard = new Scanner(System.in);
-		while(true){
-			System.out.print(" > ");
+		boolean moreCommands = true;
+		while(moreCommands){
+			System.out.print(Message.NEWCOMMAND);
 			String input = keyboard.nextLine();			
-			String [] commands = input.split("[|]");
-			for(int i = 0; i<commands.length; i++){
-				String currentCommand = commands[i].toLowerCase().trim();
-				switch(currentCommand){
-					case "pwd":
-					case "ls":
-					case "cd":
-					case "cat":
-					case "grep":
-					case "wc":
-					case "uniq":
-					case ">":
-					case "exit": System.exit(-1);
-					default: //something about invalid command
-				//call each command here
-				}
-			}
+			//Send input to Command Builder here
+			
 		}
 	}
 	
