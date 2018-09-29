c6c478d339c928fee499c7b420c678cf5eb59adf
MidgeWilliams <43554894+MidgeWilliams@users.noreply.github.com>
2018-09-29 10:50:27 -0400

    Tweaks for cat

--- src/cs131/pa1/filter/sequential/CATFilter.java
+++ src/cs131/pa1/filter/sequential/CATFilter.java
@@ -5,7 +5,13 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class CATFilter extends SequentialFilter {
-
+	//Still have to overwrite Process, how you've written this it will do it for EVERY line lol 
+	//The "line" is going to be each individual line in the file, which is fed one by one into this method
+	
+	//You also have to initialize the Queue input since that isn't done, you can probably do that at the beginning of process
+	
+	//The file name is being given to us when the user types it, so you need to make a new method to take the file and 
+	//turn it into a Queue of strings (I think each line is supposed to be a string in the queue)
 	@Override
 	protected String processLine(String line) {
 		//errors to check: if its a file, if the file exists
